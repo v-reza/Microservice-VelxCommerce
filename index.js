b93720c6f1c301b7116e4338ec4690568a9441ae
mrezalf0 <mochammad.zulfi@mail.sirclo.com>
2022-07-24 15:45:52 +0700

    init socket

---
+++ index.js
@@ -0,0 +1,53 @@
+const server = require("http").createServer();
+const options = {
+  cors: true,
+  origin: ["http://localhost:3000"],
+  origins: ["http://localhost:3000"],
+};
+
+const io = require("socket.io")(server, options);
+
+let users = [];
+
+const addUser = (userId, socketId) => {
+  !users.some((user) => user.userId === userId) &&
+    users.push({ userId, socketId });
+};
+
+const removeUser = (socketId) => {
+  users = users.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (userId) => {
+  return users.find((user) => user.userId === userId);
+};
+
+io.on("connection", (socket) => {
+  //when connect
+  console.log("a user connect");
+
+  //take userId and socketId from user
+  socket.on("addUser", (userId) => {
+    addUser(userId, socket.id);
+    io.emit("getUsers", users);
+  });
+
+  //send and get message
+  socket.on("sendMessage", ({ senderId, receiverId, text }) => {
+    const user = getUser(receiverId);
+    console.log(user)
+    io.to(user.socketId).emit("getMessage", {
+      senderId,
+      text,
+    });
+  });
+
+  //when disconnect
+  socket.on("disconnect", () => {
+    console.log("disconnect");
+    removeUser(socket.id);
+    io.emit("getUsers", users);
+  });
+});
+
+server.listen(process.env.PORT || 5000);
